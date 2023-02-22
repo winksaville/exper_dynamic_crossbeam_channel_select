67814c2e1dc91b35c209f7ac908deb5cc87c6d21
Wink Saville <wink@saville.com>
2023-02-21 21:54:43 -0800

    feat: Much simpler example showing the compile error
     $ cargo run
        Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
     error[E0502]: cannot borrow `receivers` as mutable because it is also borrowed as immutable
       --> src/main.rs:32:5
        |
     13 |     sel.recv(&receivers[0]);
        |               --------- immutable borrow occurs here
     ...
     32 |     receivers.push(rx);
        |     ^^^^^^^^^^^^^^^^^^ mutable borrow occurs here
     33 |     sel.recv(&receivers[0]);
        |     ----------------------- immutable borrow later used here
    
     For more information about this error, try `rustc --explain E0502`.
     error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Experiment dynamic crossbeam_channel select
 
 Experiment with dynamically adding/remove receivers
-from crossbeam_channel select.
+from crossbeam_channel select. This devolved into a
+compile error. This is a relatively simple example
+of the compile error.
+
+If line 33 is commented out there is no error:
+```
+$ cargo run
+   Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.22s
+     Running `target/debug/exper_dynamic_crossbeam_channel_select`
+main:+
+main:-
+```
+
+If line 33 is present then we have an error:
+```
+
+```
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,36 @@
-use crossbeam_channel::{bounded, unbounded, Receiver, Select, Sender};
-
-#[allow(unused)]
-fn simple() {
-    println!("simple:+");
-
-    std::thread::scope(|scope| {
-        println!("outer thread:+");
-
-        let (cmd_tx, cmd_rx) = unbounded::<i32>();
-        let (rsp_tx, rsp_rx) = unbounded::<Sender<i32>>();
-        let (done_tx, done_rx) = bounded::<()>(0);
-
-        let mut receiver: Vec<Receiver<i32>> = Vec::new();
-
-        scope.spawn(move || {
-            println!("t1:+");
-
-            let mut sel = Select::new();
-
-            // From cmd_rx get two i32's
-            let oper1 = sel.recv(&cmd_rx);
-            for li in 1..=2 {
-                println!("t1: TOL1");
-                let oper = sel.select();
-                match oper.index() {
-                    i if i == oper1 => {
-                        let oper1_v = oper.recv(&cmd_rx).unwrap();
-                        println!("t1: oper1_v={oper1_v}");
-                        assert_eq!(li, oper1_v);
-                    }
-                    _ => unreachable!(),
-                }
-            }
-
-            let (tx, rx) = unbounded::<i32>();
-            rsp_tx.send(tx);
-            receiver.push(rx);
-            let oper2 = sel.recv(&receiver[0]);
-
-            // Add r2 and get 2 messages
-            for _ in 3..=4 {
-                println!("t1: TOL2");
-                let oper = sel.select();
-                match oper.index() {
-                    i if i == oper1 => {
-                        let oper1_v = oper.recv(&cmd_rx).unwrap();
-                        println!("t1: oper1_v={oper1_v}");
-                        assert_eq!(oper1_v, 3);
-                    }
-                    i if i == oper2 => {
-                        let oper2_v = oper.recv(&receiver[0]).unwrap();
-                        println!("t1: oper2_v={oper2_v}");
-                        assert_eq!(oper2_v, 4);
-                    }
-                    _ => unreachable!(),
-                }
-            }
-
-            done_tx.send(());
-
-            println!("t1:-");
-        });
-
-        cmd_tx.send(1).unwrap();
-        cmd_tx.send(2).unwrap();
-        cmd_tx.send(3).unwrap();
-
-        println!("outer thread: waiting for rev other_cmd_tx");
-        let other_cmd_tx = rsp_rx.recv().unwrap();
-        println!("outer thread:    received rev other_cmd_tx");
-        other_cmd_tx.send(4).unwrap();
-        //other_cmd_tx.send(5).unwrap();
-
-        done_rx.recv().unwrap();
-        println!("outer thread:-");
-    });
-
-    println!("simple:-");
-}
+use crossbeam_channel::{unbounded, Receiver, Select};
 
 fn main() {
     println!("main:+");
-    simple();
+    type BoxMsgAny = Box<dyn std::any::Any + Send>;
+
+    let (_cmd_tx, cmd_rx) = unbounded::<BoxMsgAny>();
+
+    let mut sel = Select::new();
+    let mut receivers: Vec<Receiver<BoxMsgAny>> = Vec::new();
+
+    receivers.push(cmd_rx);
+    sel.recv(&receivers[0]);
+
+    let (_tx, rx) = unbounded::<BoxMsgAny>();
+
+    // $ cargo run
+    //    Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+    // error[E0502]: cannot borrow `receivers` as mutable because it is also borrowed as immutable
+    //   --> src/main.rs:32:5
+    //    |
+    // 13 |     sel.recv(&receivers[0]);
+    //    |               --------- immutable borrow occurs here
+    // ...
+    // 32 |     receivers.push(rx);
+    //    |     ^^^^^^^^^^^^^^^^^^ mutable borrow occurs here
+    // 33 |     sel.recv(&receivers[0]);
+    //    |     ----------------------- immutable borrow later used here
+    // 
+    // For more information about this error, try `rustc --explain E0502`.
+    // error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error
+    receivers.push(rx);
+    //sel.recv(&receivers[0]);
+
     println!("main:-");
 }
