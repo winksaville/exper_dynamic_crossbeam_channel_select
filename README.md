03bbf7acdb59f73610f192e5ccc1381d4b22b2bd
Wink Saville <wink@saville.com>
2023-02-23 12:12:41 -0800

    feat: Prove dynamic receivers are "possible"

--- README.md
+++ README.md
@@ -1,23 +1,104 @@
 # Experiment dynamic crossbeam_channel select
 
 Experiment with dynamically adding/remove receivers
-from crossbeam_channel select. This devolved into a
-compile error. This is a relatively simple example
-of the compile error.
+from crossbeam_channel select. At the moment I can't
+get this to work by incrementally creating receivers. But
+if I preallocate receivers I can incrementally add them
+to a crossbeam_channel Select object.
 
-If line 33 is commented out there is no error:
+But, I believe it's possible as shown by the "working" test in
+mutable_receivers_vec. In there I declare `receivers: Rc<RefCell<Vec<Receiver<i32>>>>`
+and incrementally add receivers and show that the address, `ref_rx`,
+are the same as the addresses printed after the loop.
 ```
+wink@3900x 23-02-23T20:00:49.951Z:~/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select (Simple-example-showing-compile-error)
 $ cargo run
    Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
-    Finished dev [unoptimized + debuginfo] target(s) in 0.22s
+    Finished dev [unoptimized + debuginfo] target(s) in 0.18s
      Running `target/debug/exper_dynamic_crossbeam_channel_select`
-main:+
-main:-
+
+add_rx_from_immutable_rcvrs: ref_rx=0x55df667baae0
+add_rx_from_immutable_rcvrs: ref_rx=0x55df667baaf0
+ok: idx=0 ref_rx=0x55df667bac30
+ok: idx=1 ref_rx=0x55df667bac40
+ok: &(*receivers).borrow()[0]=0x55df667bac30
+ok: &(*receivers).borrow()[1]=0x55df667bac40
 ```
 
-If line 33 is present then we have an error:
+I've also used "cfg" features cerr1, cerr2, cerr3 and cerr4 so
+the errors output by the different code can be seen:
+> Note you can run 2 or more cfgs by listing them; `cargo build --features cerr1,cerr2,cerr3,cerr4`
 ```
+wink@3900x 23-02-23T19:21:49.151Z:~/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select (Simple-example-showing-compile-error)
+$ cargo build --features cerr1
+   Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+error[E0716]: temporary value dropped while borrowed
+  --> src/main.rs:70:27
+   |
+70 |             let ref_rx = &(*receivers).borrow()[idx];
+   |                           ^^^^^^^^^^^^^^^^^^^^^ creates a temporary value which is freed while still in use
+71 |             println!("cerr1: idx={idx} ref_rx={ref_rx:p}");
+72 |             sel.recv(ref_rx);
+   |             ---------------- borrow later used here
+73 |         }
+   |         - temporary value is freed at the end of this statement
+   |
+   = note: consider using a `let` binding to create a longer lived value
 
+For more information about this error, try `rustc --explain E0716`.
+error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error
+wink@3900x 23-02-23T19:26:58.945Z:~/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select (Simple-example-showing-compile-error)
+$ cargo build --features cerr2
+   Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+error[E0716]: temporary value dropped while borrowed
+  --> src/main.rs:84:45
+   |
+84 |             add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx);
+   |                              --------       ^^^^^^^^^^^^^^^^^^^^^^     - temporary value is freed at the end of this statement
+   |                              |              |
+   |                              |              creates a temporary value which is freed while still in use
+   |                              borrow later used here
+   |
+   = note: consider using a `let` binding to create a longer lived value
+
+For more information about this error, try `rustc --explain E0716`.
+error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error
+wink@3900x 23-02-23T19:27:01.281Z:~/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select (Simple-example-showing-compile-error)
+$ cargo build --features cerr3
+   Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+error[E0716]: temporary value dropped while borrowed
+   --> src/main.rs:99:45
+    |
+99  |             add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx1);
+    |                                             ^^^^^^^^^^^^^^^^^^^^^^      - temporary value is freed at the end of this statement
+    |                                             |
+    |                                             creates a temporary value which is freed while still in use
+100 |             add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx2);
+    |                              -------- borrow later used here
+    |
+    = note: consider using a `let` binding to create a longer lived value
+
+For more information about this error, try `rustc --explain E0716`.
+error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error
+wink@3900x 23-02-23T19:27:02.719Z:~/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select (Simple-example-showing-compile-error)
+$ cargo build --features cerr4
+   Compiling exper_dynamic_crossbeam_channel_select v0.1.0 (/home/wink/prgs/rust/myrepos/exper_dynamic_crossbeam_channel_select)
+error[E0716]: temporary value dropped while borrowed
+   --> src/main.rs:117:30
+    |
+117 |                 let r_rx1 = &(*receivers).borrow()[0];
+    |                              ^^^^^^^^^^^^^^^^^^^^^ creates a temporary value which is freed while still in use
+...
+121 |             }
+    |             - temporary value is freed at the end of this statement
+...
+127 |                 sel.recv(r_rx2); // If enabled error[E0716] on line 23
+    |                 --------------- borrow later used here
+    |
+    = note: consider using a `let` binding to create a longer lived value
+
+For more information about this error, try `rustc --explain E0716`.
+error: could not compile `exper_dynamic_crossbeam_channel_select` due to previous error
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,148 @@
-use std::{cell::RefCell, rc::Rc};
-
 use crossbeam_channel::{unbounded, Receiver, Select};
 
-//fn add_to_sel<'a>(sel: &'a mut Select<'a>, rx: &'a Receiver<i32>) {
-//    println!("add_to_sel: rx={:p}", rx);
-//    sel.recv(rx);;
-//}
-
-fn main() {
+fn immutable_receivers_vec() {
     let mut sel = Select::new();
-
-    let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
-
-    //let (_tx1, rx1) = unbounded::<i32>();
-    //let (_tx2, rx2) = unbounded::<i32>();
 
     let (_tx1, rx1) = unbounded::<i32>();
     let (_tx2, rx2) = unbounded::<i32>();
 
+    // If rcvrs is immutable all is well
     {
-        receivers.borrow_mut().push(rx1);
-        let r_rx1 = &(*receivers).borrow()[0];
-        println!("&*r_rx1={:p}", r_rx1);
-        sel.recv(r_rx1);
-        println!("after sel.recv(r_rx1)");
+        // Pre allocate the rcvrs
+        let rcvrs = vec![rx1, rx2];
+
+        // Incrementally add them to sel
+        add_rx_from_immutable_rcvrs(&mut sel, &rcvrs, 0);
+        add_rx_from_immutable_rcvrs(&mut sel, &rcvrs, 1);
     }
 
+    fn add_rx_from_immutable_rcvrs<'a>(
+        sel: &mut Select<'a>,
+        rcvrs: &'a [Receiver<i32>],
+        idx: usize,
+    ) {
+        let ref_rx = &rcvrs[idx];
+        println!("add_rx_from_immutable_rcvrs: ref_rx={ref_rx:p}");
+        sel.recv(ref_rx);
+    }
+}
+
+/// Various techniques showing you can't use a mutable array of receivers.
+fn mutable_receivers_vec() {
+    #[allow(unused)]
+    use std::{cell::RefCell, rc::Rc};
+
+    // This proves to me that "cerr1" should work as it's identical to this
+    // except without the `let mut sel = Select::new();` and `sel.recv(ref_rx);`.
+    // The "proof" is that `Receiver<i32>` addresses printed in the loop are the
+    // same as seen after the loop is completed. Thus "proving" the `Receiver<i32>`s
+    // weren't move, released or dropped.
     {
-        receivers.borrow_mut().push(rx2);
-        let r_rx2 = &(*receivers).borrow()[1];
-        println!("&*r_rx2={:p}", &*r_rx2);
-        //sel.recv(r_rx2); // If enabled error[E0716] on line 23
-        println!("after sel.recv(r_rx2)");
+        let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
+
+        for _ in 0..=1 {
+            let (_, rx) = unbounded::<i32>();
+            {
+                receivers.borrow_mut().push(rx);
+            }
+            let idx = receivers.borrow().len() - 1;
+            let ref_rx = &((*receivers).borrow()[idx]);
+            println!("ok: idx={idx} ref_rx={ref_rx:p}");
+        }
+        println!("ok: &(*receivers).borrow()[0]={:p}", &(*receivers).borrow()[0]);
+        println!("ok: &(*receivers).borrow()[1]={:p}", &(*receivers).borrow()[1]);
     }
 
-    println!("receivers.len()={}", (*receivers).borrow().len());
+    #[cfg(feature = "cerr1")]
+    {
+        let mut sel = Select::new();
+        let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
 
-    //let receivers: RefCell<Vec<Receiver<i32>>> = RefCell::new(Vec::new());
-    //let (_tx1, rx1) = unbounded::<i32>();
-    //let (_tx2, rx2) = unbounded::<i32>();
+        // Won't compile error[E0716]
+        for _ in 0..=1 {
+            let (_, rx) = unbounded::<i32>();
+            {
+                receivers.borrow_mut().push(rx);
+            }
+            let idx = receivers.borrow().len() - 1;
+            let ref_rx = &(*receivers).borrow()[idx];
+            println!("cerr1: idx={idx} ref_rx={ref_rx:p}");
+            sel.recv(ref_rx);
+        }
+    }
 
-    //receivers.borrow_mut().push(rx1);
-    //receivers.borrow_mut().push(rx2);
+    #[cfg(feature = "cerr2")]
+    {
+        let mut sel = Select::new();
+        let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
 
-    //let r_rx1 = &receivers.borrow()[0];
-    //let r_rx2 = &receivers.borrow()[1];
+        // Won't compile error[E0716]
+        for _ in 0..=1 {
+            let (_, rx) = unbounded::<i32>();
+            add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx);
+        }
+    }
 
-    //println!("&*r_rx1={:p}", &*r_rx1);
-    //println!("&*r_rx2={:p}", &*r_rx2);
+    #[cfg(feature = "cerr3")]
+    {
+        let mut sel = Select::new();
 
-    //add_to_sel(&'a mut sel, r_rx1);
-    //sel.recv(r_rx1);
-    //sel.recv(r_rx2);
+        let (_tx1, rx1) = unbounded::<i32>();
+        let (_tx2, rx2) = unbounded::<i32>();
 
-    //// Allocate first receiver
-    //let (_tx1, rx1) = unbounded::<i32>();
-    //receivers.borrow_mut().push(rx1);
-    //let r_rx1 = &receivers.borrow()[0];
-    //println!("&*r_rx1={:p}", &*r_rx1);
-    //sel.recv(&*r_rx1);
+        let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
 
-    //// Allocate second receiver
-    //let (_tx2, rx2) = unbounded::<i32>();
-    //receivers.borrow_mut().push(rx2);
-    //let r_rx2 = &receivers.borrow()[1];
-    //println!("&*r_rx2={:p}", &*r_rx2);
-    //sel.recv(&*r_rx2);
+        // Won't compile error[E0716]
+        {
+            add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx1);
+            add_rx_mut_rcvrs(&mut sel, &mut receivers.borrow_mut(), rx2);
+        }
+    }
 
-    //let lcl_i32 = 1;
-    //println!("&lcl_i32={:p} lcl_i32={}", &lcl_i32, lcl_i32);
+    #[cfg(feature = "cerr4")]
+    {
+        let mut sel = Select::new();
 
-    //let box_i32 = Box::new(1);
-    //println!("&box_i32={:p} &*box_i32={:p} box_i32={}", &box_i32, &*box_i32, box_i32);
+        let (_tx1, rx1) = unbounded::<i32>();
+        let (_tx2, rx2) = unbounded::<i32>();
 
-    //let lcl_string = "astring".to_string();
-    //println!("&lcl_string={:p} &*lcl_string={:p} lcl_string={}", &lcl_string, &*lcl_string, lcl_string);
+        let receivers: Rc<RefCell<Vec<Receiver<i32>>>> = Rc::new(RefCell::new(Vec::new()));
 
-    //let box_string = Box::new("astring".to_string());
-    //println!("&box_string={:p} &*box_string={:p} box_string={}", &box_string, &*box_string, box_string);
+        // Won't compile error[E0716]
+        {
+            {
+                receivers.borrow_mut().push(rx1);
+                let r_rx1 = &(*receivers).borrow()[0];
+                println!("&*r_rx1={:p}", r_rx1);
+                sel.recv(&*r_rx1);
+                println!("after sel.recv(r_rx1)");
+            }
 
-    //// Allocating two receivers in a loop does not compile!
-    //for i in 0..1 {
-    //    // Allocate next receiver sometime in the future
-    //    let (_tx2, rx2) = unbounded::<i32>();
-    //    receivers.borrow_mut().push(rx2);
+            {
+                receivers.borrow_mut().push(rx2);
+                let r_rx2 = &(*receivers).borrow()[1];
+                println!("&*r_rx2={:p}", &*r_rx2);
+                sel.recv(r_rx2); // If enabled error[E0716] on line 23
+                println!("after sel.recv(r_rx2)");
+            }
+        }
+    }
 
-    //    println!("{i}: {:?}", receivers.borrow().len());
-    //    println!("&receivers={:p} receivers={:?}", &receivers, receivers);
-    //    println!("&receivers.borrow()={:p} &*receivers.borrow()={:p} &receivers.borrow()[0]={:p}", &receivers.borrow(), &*receivers.borrow(), &receivers.borrow()[0]);
+    #[allow(unused)]
+    fn add_rx_mut_rcvrs<'a>(
+        sel: &mut Select<'a>,
+        mut_rcvrs: &'a mut Vec<Receiver<i32>>,
+        rx: Receiver<i32>,
+    ) {
+        let idx = mut_rcvrs.len();
+        mut_rcvrs.push(rx);
+        let ref_rx = &mut_rcvrs[idx];
+        sel.recv(ref_rx);
+    }
 
-    //    for i in 0..receivers.borrow().len() {
-    //        let x = receivers.borrow();
-    //        println!("receivers.borrow()[{i}] {:p}={:?}", &x[i], x[i]);
-    //    }
-    //    let rx = &receivers.borrow()[i];
-    //    println!("{i}: &rx={:p} &*rx={:p} {:?}", &rx, &*rx, rx);
-    //    sel.recv(&*rx);
-    //}
+}
+
+fn main() {
+    immutable_receivers_vec();
+    mutable_receivers_vec();
 }
